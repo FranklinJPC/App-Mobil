121f665a0b75874f73d6056577875a095c6a3199
FranklinJPC <franklin.patino@epn.edu.ec>
2023-11-15 15:50:26 -0500

    initi

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// import {} from @ionic/pwa
 
 if (environment.production) {
   enableProdMode();
